d48e90276571db9b87a7bd18ddea4df30c100981
jaxkyoung <jaxkyoung@icloud.com>
2021-12-18 01:33:42 +0000

    removed isLoggedIn function

--- src/dbAccess.py
+++ src/dbAccess.py
@@ -1,17 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, current_user
 # encyption library
 import bcrypt
 from getpass import getpass
 
 '''Class to create, read, update, verify, and delete user profiles'''
 class userAccess(object):
-
-    def isLoggedIn():
-        if current_user.is_authenticated:
-            return True
-        else: 
-            return False
 
     # function to add user to json DB, requires valid email, password, first name and surname
     def addPendingUser(db, PendingUser, email, password, fName, sName, userType):
@@ -53,19 +45,23 @@ class userAccess(object):
         db.session.add(user)
         db.session.commit()
 
+    # returns list of users in user table
     def getUserDetails(User):
         users = User.query.all()
         return users
- 
+    
+    # returns list of pending users in pendinguser table
     def getPendingUserDetails(PendingUser):
         users = PendingUser.query.all()
         return users
 
+    # returns hashed password when plain text password is passed to it
     def get_hashed_password(plain_text_password):
         # Hash a password for the first time
         # (Using bcrypt, the salt is saved into the hash itself)
         return bcrypt.hashpw(plain_text_password.encode(), bcrypt.gensalt())
 
+    # returns boolena when passed with email and plain text password
     def check_password(User, email, plain_text_password):
         users = userAccess.getUserDetails(User)
         for user in users:
@@ -74,11 +70,8 @@ class userAccess(object):
         # Check hashed password. Using bcrypt, the salt is saved into the hash itself
         return bcrypt.checkpw(plain_text_password.encode(), hashed_password.encode())
 
-    def checkUser(email, password):
-        pass
-
     def getUser(User, email):
-        users = User.query.filter_by(email = email)
+        users = User.query.filter_by(email = email).first()
         return users
 
     def getUserName(User, email):
@@ -89,8 +82,7 @@ class userAccess(object):
                 return user.fName, user.sName
 
     def getUserFirstName(User, current_user):
-        loggedIn = userAccess.isLoggedIn()
-        if loggedIn:
+        if current_user.is_authenticated():
             user = User.query.filter_by(id=current_user.id).first()
             userFirstName = user.fName
         else:
