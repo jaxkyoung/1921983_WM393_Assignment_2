f2d39e0d62d83ec3c5a0de1b695a8d719581fac2
jaxkyoung <jaxkyoung@icloud.com>
2021-12-19 20:23:24 +0000

    added question and answer functions

--- src/dbBoardAccess.py
+++ src/dbBoardAccess.py
@@ -1,6 +1,15 @@
 # encyption library
 import bcrypt
 from getpass import getpass
+from flask_login import current_user
+from datetime import datetime
+
+# datetime object containing current date and time
+def getDateTime():
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    return now
 
 '''Class to create, read, update, verify, and delete user profiles'''
 class boardAccess(object):
@@ -21,92 +30,36 @@ class boardAccess(object):
         boards = QABoard.query.all()
         return boards
 
-    # function to add user to json DB, requires valid email, password, first name and surname
-    def addUser(db, User, email, password, fName, sName, userType):
-        # hash the input password
-        hash = userAccess.get_hashed_password(password)
-        # convert hash to string
-        hash = str(hash)
-        # extract raw hash
-        hash = hash.split("'")
-        hash = hash[1]
-        # form dictionary to be appended to json DB
-        user = User(   
-            email = email, 
-            password = hash, 
-            fName = fName, 
-            sName = sName,
-            userType = userType
-        )
-        db.session.add(user)
+    def editBoard(db, QABoard, boardId, boardName):
+        board = QABoard.query.filter_by(id=boardId).first()
+        board.boardName = boardName
         db.session.commit()
 
-    # returns list of users in user table
-    def getUserDetails(User):
-        users = User.query.all()
-        return users
-    
-    # returns list of pending users in pendinguser table
-    def getPendingUserDetails(PendingUser):
-        users = PendingUser.query.all()
-        return users
+    def addQuestion(db, Question, qTitle, qBody, boardId, posterId=current_user.id):
+        now = getDateTime()
+        new_question = Question(
+            qTitle = qTitle,
+            qBody = qBody,
+            postDate = now,
+            posterId = posterId,
+            boardId = boardId
+        )
+        db.session.add(new_question)
+        db.session.commit()
 
-    # returns hashed password when plain text password is passed to it
-    def get_hashed_password(plain_text_password):
-        # Hash a password for the first time
-        # (Using bcrypt, the salt is saved into the hash itself)
-        return bcrypt.hashpw(plain_text_password.encode(), bcrypt.gensalt())
+    def deleteQuestion(db, Question, questionId):
+        question = Question.query.filter_by(id=questionId).first()
+        db.session.delete(question)
+        db.session.commit()
 
-    # returns boolena when passed with email and plain text password
-    def check_password(User, email, plain_text_password):
-        users = userAccess.getUserDetails(User)
-        for user in users:
-            if user.email == email:
-                hashed_password = user.password
-        # Check hashed password. Using bcrypt, the salt is saved into the hash itself
-        return bcrypt.checkpw(plain_text_password.encode(), hashed_password.encode())
-
-    def getUser(User, email):
-        users = User.query.filter_by(email = email).first()
-        return users
-
-    def getUserName(User, email):
-        users = userAccess.getUserDetails(User)
-        # Iterating through the json user detials until matching details found
-        for user in users:
-            if user.email == email:
-                return user.fName, user.sName
-
-    def getUserType(User, email):
-        users = userAccess.getUserDetails(User)
-        # Iterating through the json user detials until matching details found
-        for user in users:
-            if user.email == email:
-                return user.userType
-
-    def approveUser(db, PendingUser, User, email):
-        users = userAccess.getPendingUserDetails(PendingUser)
-        for user in users:
-            if user.email == email:
-                user_to_approve = User(
-                    email = user.email, 
-                    password = user.password, 
-                    fName = user.fName, 
-                    sName = user.sName, 
-                    userType = user.userType
-                )
-                db.session.delete(user)
-                db.session.add(user_to_approve)
-                db.session.commit()
-
-    def denyUser(db, PendingUser, email):
-        users = userAccess.getPendingUserDetails(PendingUser)
-        for user in users:
-            if user.email == email:
-                db.session.delete(user)
-                db.session.commit()
-
-#userAccess.addPendingUser("john.smith@warwick.ac.uk", "example", "John", "Smith", "student")
-# users = PendingUser.query.all()
-# for user in users:
-#     print(user.email)
+    def addAnswer(db, Answer, aBody, questionId, posterId=current_user.id):
+        now = getDateTime()
+        new_answer = Answer(
+            aBody = aBody,
+            postDate = now,
+            posterId = posterId,
+            questionId = questionId
+        )
+        db.session.add(new_answer)
+        db.session.commit()
+        
