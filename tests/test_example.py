5dcd1c73e24d53e4b89c9e908608d1cd76966474
jaxkyoung <jaxkyoung@icloud.com>
2021-12-23 21:45:42 +0000

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,5 +6,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
+import app.app as app
+
 def test_always_pass():
     assert True
