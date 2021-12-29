6a156935753e10646484add0e6ea83ef38f443cf
jaxkyoung <jaxkyoung@icloud.com>
2021-12-29 00:33:21 +0000

    added DB URL to productionconfig

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class ProductionConfig:
     SECRET_KEY = 'this is the secret key for the production app'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///WMGTSS.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://qtcllstuqtgix8mi:ckcs304toki4fkvb@yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ap7ehx04x2fx13c4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
 
