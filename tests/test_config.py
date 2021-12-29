84e9794c09ed4eb48a0d1d571e512a39f3a42ec0
jaxkyoung <jaxkyoung@icloud.com>
2021-12-29 00:10:18 +0000

    updated path to config files

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,18 @@
 import os
 def test_development_config(app):
-    app.config.from_object('project.config.DevelopmentConfig')
+    app.config.from_object('app.config.DevelopmentConfig')
     assert app.config['DEBUG']
     assert not app.config['TESTING']
     assert app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get(
         'DATABASE_URL')
 def test_testing_config(app):
-    app.config.from_object('project.config.TestingConfig')
+    app.config.from_object('app.config.TestingConfig')
     assert app.config['DEBUG']
     assert app.config['TESTING']
-    assert not app.config['PRESERVE_CONTEXT_ON_EXCEPTION']
     assert app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get(
         'DATABASE_TEST_URL')
 def test_production_config(app):
-    app.config.from_object('project.config.ProductionConfig')
+    app.config.from_object('app.config.ProductionConfig')
     assert not app.config['DEBUG']
     assert not app.config['TESTING']
     assert app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get(
