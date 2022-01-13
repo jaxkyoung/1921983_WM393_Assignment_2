6dec639c74d69c01b9bfdeea2aba71f73db8bf90
jaxkyoung <jaxkyoung@icloud.com>
2022-01-13 09:24:11 +0000

    Updated config to include testing param

--- app/config.py
+++ app/config.py
@@ -3,12 +3,14 @@ class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql://qtcllstuqtgix8mi:ckcs304toki4fkvb@yvu4xahse0smimsc.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ap7ehx04x2fx13c4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
+    TESTING = False
 
 class DevelopmentConfig:
     SECRET_KEY = 'dev'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///WMGTSS.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+    TESTING = False
 
 class TestingConfig:
     SECRET_KEY = 'test'
