b77d99cc5cd37fc9d3493c5089ccf7bcdd2988ae
jaxkyoung <jaxkyoung@icloud.com>
2022-01-13 09:24:25 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
+'''
+run.py: Used to run development server on local host
+version: 1.0
+author: 1921983
+'''
+# import create app factory from app package
 from app import create_app
+
+# create app
 app = create_app()
+# set app config to dev config
 app.config.from_object('app.config.DevelopmentConfig')
+
+# run app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
