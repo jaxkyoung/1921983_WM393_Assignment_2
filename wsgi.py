f04e1b76b928ed93867ed0ff3ad80675147a4a6c
jaxkyoung <jaxkyoung@icloud.com>
2022-01-13 09:24:18 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import create_app
+
 app = create_app()
 app.config.from_object('app.config.ProductionConfig')
 app.app_context().push()
+
 if __name__ == "__main__":
     app.run(debug=True)
