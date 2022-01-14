320f6ff71ea476ee4277321d0a2a630efac4687a
jaxkyoung <jaxkyoung@icloud.com>
2022-01-14 14:31:11 +0000

    Delete test_ping.py

--- tests/test_ping.py
+++
@@ -1,8 +0,0 @@
-import json
-def test_ping(app):
-    client = app.test_client()
-    resp = client.get('/ping')
-    data = json.loads(resp.data.decode())
-    assert resp.status_code == 200
-    assert 'pong' in data['message']
-    assert 'success' in data['status']
