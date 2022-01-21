97cb081e0e37e3052ede09785eefaba9004fbb36
jaxkyoung <jaxkyoung@icloud.com>
2022-01-21 18:57:02 +0000

    added q&a board home test

--- tests/test_boardRoutes.py
+++ tests/test_boardRoutes.py
@@ -1,4 +1,14 @@
 import json
+
+def login(client, email, password):
+    return client.post('/log-in', data=dict(
+        userEmail=email,
+        userPassword=password
+    ), follow_redirects=True)
+
+def logout(client):
+    return client.get('/logged-out', follow_redirects=True)
+
 def test_ping(app):
     client = app.test_client()
     resp = client.get('/ping')
@@ -10,4 +20,17 @@ def test_ping(app):
 def test_home(app):
     client = app.test_client()
     resp = client.get('/')
+    assert resp.status_code == 200
+
+def test_QABoardHome(app):
+    client = app.test_client()
+    resp = client.get('/Q-A-Board')
+    assert b"Q&A Boards" not in resp.data
+    assert resp.status_code == 302
+    
+    email = 'tutor@warwick.ac.uk'
+    password = 'tutor'
+    login(client, email, password)
+    resp = client.get('/Q-A-Board')
+    assert b"Q&A Boards" in resp.data
     assert resp.status_code == 200
